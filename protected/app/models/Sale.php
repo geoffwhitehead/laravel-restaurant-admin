7957cb4b48c4fe4244155990aad17e06c0a9c4cf
geoffwhitehead <geoff1012@gmail.com>
2015-04-11 01:04:16 +0100

    some sale stuff

--- protected/app/models/Sale.php
+++ protected/app/models/Sale.php
@@ -1,27 +1,34 @@
 <?php
-class Sale extends BaseModel  {
-	
-	protected $table = 'sales';
-	protected $primaryKey = 'id';
 
-	public function __construct() {
-		parent::__construct();
-		
-	}
+class Sale extends BaseModel
+{
 
-	public static function querySelect(  ){
-		
-		
-		return "  SELECT sales.* FROM sales  ";
-	}
-	public static function queryWhere(  ){
-		
-		return " WHERE sales.id IS NOT NULL   ";
-	}
-	
-	public static function queryGroup(){
-		return "  ";
-	}
-	
+    protected $table = 'sales';
+    protected $primaryKey = 'id';
 
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public static function querySelect()
+    {
+
+        return "  SELECT sales.* FROM sales  ";
+    }
+
+    public static function queryWhere()
+    {
+        if (Session::get('gid') > 2) {
+            return "WHERE id = (select max(id) from sales where site_id = ".Session::get("sid").")";
+        } else {
+            return " WHERE sales.id IS NOT NULL   ";
+        }
+    }
+
+    public static function queryGroup()
+    {
+        return "  ";
+    }
 }
